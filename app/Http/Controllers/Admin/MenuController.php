d973f24471e01f7414e0a5fe30b4fbe10562830f
karam27 <karamrafeek2001@gmail.com>
2023-08-07 21:45:41 +0300

    Controllers  update

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Menu;
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
@@ -12,7 +13,8 @@ class MenuController extends Controller
      */
     public function index()
     {
-       return view('admin.menus.index');
+        $menus = Menu::all();
+        return view('admin.menus.index', compact('menus'));
     }
 
     /**
