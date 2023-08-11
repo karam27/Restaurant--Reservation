eebf5f295e76ac989dd5eb2946b8c49fafdc8960
karam27 <karamrafeek2001@gmail.com>
2023-08-11 18:10:04 +0300

    Table controller

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TableStoreRequest;
 use App\Models\Table;
-use Illuminate\Http\Request;
+
+
+
 
 class TableController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
@@ -20,6 +24,8 @@ class TableController extends Controller
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -28,47 +34,68 @@ class TableController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(TableStoreRequest $request)
     {
         Table::create([
             'name' => $request->name,
             'guest_number' => $request->guest_number,
-            'stauts' => $request->status,
+            'status' => $request->status,
             'location' => $request->location,
         ]);
-        return to_route('admin.tables.index');
+
+        return to_route('admin.tables.index')->with('success', 'Table created successfully.');
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function show(string $id)
+    public function show($id)
     {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function edit(string $id)
+    public function edit(Table $table)
     {
-        //
+        return view('admin.tables.edit', compact('table'));
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, string $id)
+    public function update(TableStoreRequest $request, Table $table)
     {
-        //
+        $table->update($request->validated());
+
+        return to_route('admin.tables.index')->with('success', 'Table updated successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(string $id)
+    public function destroy(Table $table)
     {
-        //
+     $table->delete();
+
+        return to_route('admin.tables.index')->with('danger', 'Table daleted successfully.');
     }
 }
