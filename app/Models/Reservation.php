6de86a89ee0a1e7b899214078e298748875c6222
karam27 <karamrafeek2001@gmail.com>
2023-08-11 22:36:26 +0300

    Reservation model

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'tel_number',
+        'email',
+        'table_id',
+        'res_date',
+        'guest_number'
+    ];
+    protected $dates = ['res_date' => TableStatus::class, 'location' => TableLocation::class];
+    public function table()
+    {
+        return $this->belongsTo(Table::class);
+    }
 }
