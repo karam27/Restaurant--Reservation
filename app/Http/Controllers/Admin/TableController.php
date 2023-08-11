c72f935965f60c94dec9a2265d8a482690860d5c
karam27 <karamrafeek2001@gmail.com>
2023-08-11 11:31:45 +0300

    Table controller

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TableStoreRequest;
 use App\Models\Table;
 use Illuminate\Http\Request;
 
@@ -28,9 +29,15 @@ class TableController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TableStoreRequest $request)
     {
-        //
+        Table::create([
+            'name' => $request->name,
+            'guest_number' => $request->guest_number,
+            'stauts' => $request->status,
+            'location' => $request->location,
+        ]);
+        return to_route('admin.tables.index');
     }
 
     /**
