cf0e6f3107c646de3de9b1db2a9aa66304317471
karam27 <karamrafeek2001@gmail.com>
2023-08-16 10:19:29 +0300

    web php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\MenuController;
 use App\Http\Controllers\Admin\ReservationController;
 use App\Http\Controllers\Admin\TableController;
+use App\Http\Controllers\Forntend\CategoryController as ForntendCategoryController;
+use App\Http\Controllers\Forntend\MenuController as ForntendMenuController;
+use App\Http\Controllers\Forntend\ReseravtionController;
+use App\Http\Controllers\Forntend\WelcomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +26,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/categoies', [ForntendCategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/{category}', [ForntendCategoryController::class, 'show'])->name('categories.show');
+Route::get('/menus', [ForntendMenuController::class, 'index'])->name('menus.index');
+Route::get('/reservation/step-one', [ReseravtionController::class, 'stepOne'])->name('reservations.step.one');
+Route::post('/reservation/step-one', [ReseravtionController::class, 'storeStepOne'])->name('reservations.store.step.one');
+Route::get('/reservation/step-two', [ReseravtionController::class, 'stepTwo'])->name('reservations.step.two');
+Route::post('/reservation/step-two', [ReseravtionController::class, 'storeStepTwo'])->name('reservations.store.step.two');
+Route::get('/thankyou', [WelcomeController::class, 'thankyou'])->name('thankyou');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
